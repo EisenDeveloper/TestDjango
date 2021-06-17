d0ee45ce4625716d3a120305f9721cca35407797
Your Name <you@example.com>
2021-06-17 19:13:46 -0400

    models 1.0

--- core/models.py
+++ core/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
 # Create your models here.
+
+from django.contrib.auth.models import User
+
+class  Usuarios(models.Model):
+    id = models.IntegerField(primary_key=True,verbose_name='Id',null = True)
+    nombre = models.CharField(max_length=15,verbose_name='Nombre',null = True)
+    correo = models.EmailField(max_length=200,verbose_name='Email',null = True)
+    genero = models.CharField(max_length=9,verbose_name='genero',null = True)
+    edad = models.IntegerField(verbose_name='edad',null = True)
+    artista = models.BooleanField(verbose_name='artista',null = True)
+    def __str__(self):
+        return self.nombre
+
+class  Contacto(models.Model):
+    email       = models.EmailField(primary_key=True,verbose_name='email',null = True)
+    titulo      = models.CharField(max_length=128,verbose_name='titulo',null = True)
+    descripcion = models.CharField(max_length=500,verbose_name='descripcion',null = True)       
+    def __str__(self):   
+        return self.titulo
+
+class Categoria(models.Model):
+    idCategoria=models.IntegerField(primary_key=True,verbose_name='Id Categoria',null = True)
+    tipoCategoria = models.CharField(max_length=60,verbose_name='tipocategoria',null = True)
+    def __str__(self):   
+        return self.tipoCategoria
+
+class  Periodistas(models.Model):
+    id_Periodista = models.IntegerField(primary_key=True,verbose_name='Id Periodista',null = True)
+    nombre = models.CharField(max_length=15,verbose_name='nombre',null = True)
+    ap_Paterno = models.CharField(max_length=15,verbose_name='Apellido Paterno',null = True)
+    ap_Materno = models.CharField(max_length=15,verbose_name='Apellido Materno',null = True)
+    tipo_Periodista = models.CharField(max_length=60,verbose_name='Tipo Periodista',null = True)
+    fecha = models.DateField(verbose_name='fecha',null = True)
+    imagen = models.ImageField(null=True,blank=True,verbose_name='imagen')
+    def __str__(self):   
+        return self.id_Periodista
