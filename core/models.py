8807a0279cbafdcaa9acf4903a8b7289ef850411
Your Name <you@example.com>
2021-06-17 19:54:39 -0400

    Actualización Models

--- core/models.py
+++ core/models.py
@@ -7,32 +7,33 @@ from django.contrib.auth.models import User
 class  Usuarios(models.Model):
     id = models.IntegerField(primary_key=True,verbose_name='Id',null = True)
     nombre = models.CharField(max_length=15,verbose_name='Nombre',null = True)
-    correo = models.EmailField(max_length=200,verbose_name='Email',null = True)
+    correo = models.EmailField(max_length=50,verbose_name='Email',null = True)
     genero = models.CharField(max_length=9,verbose_name='genero',null = True)
-    edad = models.IntegerField(verbose_name='edad',null = True)
-    artista = models.BooleanField(verbose_name='artista',null = True)
+    edad = models.IntegerField(verbose_name='Edad',null = True)
+    proofesion_Periodista = models.BooleanField(verbose_name='prof_periodista',null = False)
     def __str__(self):
-        return self.nombre
+        return self.id
 
 class  Contacto(models.Model):
     email       = models.EmailField(primary_key=True,verbose_name='email',null = True)
-    titulo      = models.CharField(max_length=128,verbose_name='titulo',null = True)
+    titulo      = models.CharField(max_length=128,verbose_name='Titulo',null = True)
     descripcion = models.CharField(max_length=500,verbose_name='descripcion',null = True)       
     def __str__(self):   
-        return self.titulo
+        return self.email
 
 class Categoria(models.Model):
     idCategoria=models.IntegerField(primary_key=True,verbose_name='Id Categoria',null = True)
     tipoCategoria = models.CharField(max_length=60,verbose_name='tipocategoria',null = True)
     def __str__(self):   
-        return self.tipoCategoria
+        return self.idCategoria
 
 class  Periodistas(models.Model):
-    id_Periodista = models.IntegerField(primary_key=True,verbose_name='Id Periodista',null = True)
+    idPeriodista = models.IntegerField(primary_key=True,verbose_name='Id Periodista',null = True)
     nombre = models.CharField(max_length=15,verbose_name='nombre',null = True)
     ap_Paterno = models.CharField(max_length=15,verbose_name='Apellido Paterno',null = True)
     ap_Materno = models.CharField(max_length=15,verbose_name='Apellido Materno',null = True)
-    tipo_Periodista = models.CharField(max_length=60,verbose_name='Tipo Periodista',null = True)
+    edad_Periodista = models.IntegerField(verbose_name='Edad Periodista', null=False)
+    idCategoria = models.ForeignKey(verbose_name='Id Categoria',null = True)
     fecha = models.DateField(verbose_name='fecha',null = True)
     imagen = models.ImageField(null=True,blank=True,verbose_name='imagen')
     def __str__(self):   
