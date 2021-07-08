913fd21a2fd7821c195ce4047c47b7b5692d9fb6
Your Name <you@example.com>
2021-07-08 01:06:45 -0400

    migración Noticia

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Periodistas, Categoria, Contacto, Usuarios
+from .models import Noticia, Periodistas, Categoria, Contacto, Usuarios
 
 # Register your models here.
 
@@ -7,3 +7,4 @@ admin.site.register(Periodistas)
 admin.site.register(Usuarios)
 admin.site.register(Categoria)
 admin.site.register(Contacto)
+admin.site.register(Noticia)
